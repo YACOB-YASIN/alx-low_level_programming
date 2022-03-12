81747a9768ec041869902ca3663188f56bc91b2d
Yacob Yasin <yasyacob1@gmail.com>
2022-03-12 10:19:19 -0800

    Task 0

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - Entry point
+ *
+ * Description: tests whether a random number is positive or negative
+ *
+ * Return: Always 0(Success)
+ */
+int main(void)
+
+	int n;
+{
