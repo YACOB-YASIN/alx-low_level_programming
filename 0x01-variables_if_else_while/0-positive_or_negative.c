554ab9123b779ca9631c9dcfa0d14c092ff292a6
YACOB-YASIN <99547396+YACOB-YASIN@users.noreply.github.com>
2022-03-14 19:51:41 +0300

    Task 0

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -2,11 +2,11 @@
 #include <stdlib.h>
 #include <time.h>
 /**
-* main - Entry point
-* Description: Get a random number and print the number
-* and if it is positive, negative, or zero
-* Return: 0 (Success)
-*/
+ * main - Entry point
+ * Description: Get a random number and print the number
+ * and if it is positive, negative, or zero
+ * Return: 0 (Success)
+ */
 
 int main(void)
 {
