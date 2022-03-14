e3fe285e918991c90b3b9efb43705b6714afe643
YACOB-YASIN <99547396+YACOB-YASIN@users.noreply.github.com>
2022-03-14 20:54:54 +0300

    Task 5

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -1,1 +1,16 @@
-fgd
+#include <stdio.h>
+/**
+ * main - main block
+ * 
+ * Return: 0
+ */
+int main(void)
+{
+	int a; 
+    
+    for (a = 0; a <10; a++)
+         printf("%d", a);
+	
+	putchar('\n');
+	return (0);
+}
