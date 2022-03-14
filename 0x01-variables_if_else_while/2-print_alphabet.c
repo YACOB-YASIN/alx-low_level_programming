de4b555ae37ff090db5ce4189cdaac5abc1022f0
YACOB-YASIN <99547396+YACOB-YASIN@users.noreply.github.com>
2022-03-14 20:19:14 +0300

    Task 2

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,20 +1,20 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <time.h>
-
 /**
- * Main - Entry point
- * Description:a program that prints the alphabet in lowercase, followed by a new line.
- * Return: Always 0
+ * main - main block
+ * Description: Get a random number and check its last digit, compare it with 5
+ * Return: 0
  */
- 
- int main(void)
- {
-     char low;
-     
-     for (low = 'a';low <= 'z'; low++)
-         putchar(low);
-         putchar('\n');
-         
-         return (0);
- }
+int main(void)
+{
+	char c = 'a';
+
+	while (c <= 'z')
+	{
+		putchar(c);
+		c++;
+	}
+
+	putchar('\n');
+	return (0);
+}
