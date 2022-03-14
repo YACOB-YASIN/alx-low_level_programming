f9223d4c9bd6ed16d1af45740970392c619cec7c
YACOB-YASIN <99547396+YACOB-YASIN@users.noreply.github.com>
2022-03-14 21:36:55 +0300

    Task 8

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -1,24 +1,34 @@
 
- #include <stdio.h>
+ 
+#include <stdio.h>
 
 /**
- * main - Entry point
- * Return:0
- */
+* main - prints all the number of base 16 in lowercase,followed by a new line.
+*
+* You can only use the putchar function (every other
+*	 function (printf, puts, etc…) is forbidden)
+* All your code should be in the main function
+* You can only use putchar three times in your code
+* Return: 0
+*/
+
 int main(void)
 {
-	int i, y;
-	for (i = 0; i < 10; i++)
-	{
-		putchar (i + '0');
-	}
-	for (y = 'a'; y <= 'f'; y++)
-	{
-		putchar (y);
-	}
+
+int n;
+char l;
+
+for (n = '0'; n <= '9'; n++)
+{
+putchar(n);
+}
+
+for (l = 'a'; l <= 'f'; l++)
+{
+putchar(l);
+}
 
 putchar('\n');
 
 return (0);
 }
-
