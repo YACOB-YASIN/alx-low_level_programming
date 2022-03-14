5ea926a2494a21dc26262ae521673742f9df571e
YACOB-YASIN <99547396+YACOB-YASIN@users.noreply.github.com>
2022-03-14 20:14:43 +0300

    Task 2

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,1 +1,20 @@
-dfshdg
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * Main - Entry point
+ * Description:a program that prints the alphabet in lowercase, followed by a new line.
+ * Return: Always 0
+ */
+ 
+ int main(void)
+ {
+     char low;
+     
+     for (low + 'a';low <= 'z'; low++)
+         putchar(low);
+         putchar('\n');
+         
+         return (0);
+ }
