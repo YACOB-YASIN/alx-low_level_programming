007a37db05a75cc17789868563ba966e9c9e9e5a
YACOB-YASIN <99547396+YACOB-YASIN@users.noreply.github.com>
2022-03-29 12:23:34 +0300

    Task 0

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,22 +1,23 @@
-#include "main.h"
+#include "holberton.h"
 
 /**
- * *_memset - fills memory with a constant byte.
- * @s: pointer to put the constant
- * @b: constant
- * @n: max bytes to use
- * Return: s
+ * _memset - function that fills memory with a constant byte.
+ *
+ * @s: mamory area to return
+ * @b: constant byte
+ * @n: size of bytes
+ * Return: char
  */
-
 
 char *_memset(char *s, char b, unsigned int n)
 {
-unsigned int i;
+	unsigned int i = 0;
 
-for (i = 0; n > 0 ; i++, n--)
-{
-	s[i] = b;
-}
+	while (i < n)
+	{
 
-return (s);
+		*(s + i) = b;
+		i += 1;
+	}
+	return (s);
 }
